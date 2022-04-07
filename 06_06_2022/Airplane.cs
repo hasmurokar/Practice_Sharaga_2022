26345d0218e7b720fbc1a3f609fc52049a7d46c2
margaritakaiki@outlook.com <margaritakaiki@outlook.com>
2022-04-07 13:37:17 +0300

    add task 06_04

---
+++ 06_06_2022/Airplane.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace _06_06_2022
+{
+    public struct Airplane
+    {
+        private string Destination { get; set; }
+        private DateTime DepartureTime { get; set; }
+        private int FreePlaces { get; set; }
+
+        public Airplane(string destination, DateTime departureTime, int freePlaces)
+        {
+            Destination = destination;
+            DepartureTime = departureTime;
+            FreePlaces = freePlaces;
+        }
+        private static void CheckTime(DateTime time)
+        {
+            if (time )
+            {
+
+            }
+        }
+        private static void CheckDestination(string destination)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Выберите пункт назначения:\nКитай, Пекин(С); Россия, Спб(R); Молдова, Кишинёв(M); Украина, Киев(U);\nРумыния, Бухарест(P); Англия, Лондон(E); Германия, Берлин(G)");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.C:
+                        if (destination.Contains("Китай"))
+                        {
+                            
+                        }
+                        break;
+                    case ConsoleKey.R:
+
+                        break;
+                    case ConsoleKey.M:
+
+                        break;
+                    case ConsoleKey.U:
+
+                        break;
+                    case ConsoleKey.P:
+
+                        break;
+                    case ConsoleKey.E:
+
+                        break;
+                    case ConsoleKey.G:
+
+                        break;
+                    default:
+                        Console.WriteLine("Неверная буква, повторите попытку");
+                        break;
+                }
+            }
+        }
+        private static void CheckAvailable()
+        {
+            
+        }
+        public override string ToString()
+        {
+            return $"Пункт назначения: {Destination}  Время отправления: {DepartureTime} Свободные места: {FreePlaces}";
+        }
+    }
+}
---
+++ 06_06_2022/Program.cs
@@ -0,0 +1,58 @@
+﻿using _06_06_2022;
+using System;
+using System.Collections.Generic;
+
+namespace _06_04_2022
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+        public static List<Airplane> airplanes = new List<Airplane>();
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 25; i++)
+            {
+                var destination = RandomDestination();
+                var departureTime = RandomDateTime();
+                var freePlaces = rnd.Next(0, 10);
+                airplanes.Add(new Airplane(destination, departureTime, freePlaces));
+            }
+            Console.WriteLine("Cписок всех билетов");
+            foreach (var item in airplanes)
+            {
+                Console.WriteLine(item);
+            }
+            if (airplanes.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+            }
+        }
+        public static string RandomDestination()
+        {
+            var array = new string[7];
+            array[0] = "Китай, Пекин";
+            array[1] = "Россия, СПб";
+            array[2] = "Молдова, Кишинёв";
+            array[3] = "Украина, Киев";
+            array[4] = "Румыния, Бухарест";
+            array[5] = "Англия, Лондон";
+            array[6] = "Германия, Берлин";
+            string result = array[rnd.Next(0, array.Length - 1)];
+            return result;
+        }
+
+        public static DateTime RandomDateTime(DateTime? min = null, DateTime? max = null)
+        {
+            min = min ?? new DateTime(2022, 04, 07);
+            max = max ?? new DateTime(2022, 04, 14);
+
+            var range = max.Value - min.Value;
+            var randomUpperBound = (Int32)range.TotalMinutes;
+            if (randomUpperBound <= 0)
+                randomUpperBound = rnd.Next(1, Int32.MaxValue);
+
+            var randTimeSpan = TimeSpan.FromMinutes((Int64)(range.TotalMinutes - rnd.Next(0, randomUpperBound)));
+            return min.Value.Add(randTimeSpan);
+        }
+    }
+}
